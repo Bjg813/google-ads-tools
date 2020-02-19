bdaba229720bfa7b0c05b846e7890bce7ba5005f
Brennan Grout <brennangrout@Damselfly.local>
2020-02-19 12:58:00 -0800

    created GoogleAdsClient constructor

--- js/clientConstructor.js
+++ js/clientConstructor.js
@@ -1,10 +1,29 @@
-function GoogleAdsClient(monthlyBudget, costInput, adjustBudget, daysLeftInMonth, roundNumber) {
-    this.monthlyBudget = monthlyBudget;
-    this.costInput = costInput;
-    this.adjustBudget = adjustBudget;
-    this.daysLeftInMonth = daysLeftInMonth();
-    this.roundNumber = roundNumber;
+// Google Ads Client constructor
+function GoogleAdsClient(params) {
+    this.monthlyBudget = params.monthlyBudget;
+    this.costInput = params.costInput;
+    this.adjustBudget = function() {
+        return (this.monthlyBudget - this.costInput) / daysLeftInMonth();
+    };
+    this.roundNumber = function() {
+        return Math.round(this.adjustBudget * 100) / 100;
+    };
 }
 
+// Cetpa Parameters - monthlyBudget, costInput
+const cetpaParams = {
+    monthlyBudget: 500,
+    costInput: document.getElementById("costInputCETPA").value
+};
 
+// Create a new constructor for cetpa client
+const cetpa = new GoogleAdsClient(cetpaParams);
 
+// List clients
+const clients = [cetpa];
+
+// Iterate through the clients
+for (let i = 0; i < clients.length; i++) {
+    clients[i].adjustBudget;
+    clients[i].roundNumber;
+}
