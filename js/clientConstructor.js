fb2cd350f5fcb0e2a1c7372ca4ca4936f8e4e91d
Brennan Grout <brennan@Brennans-iMac.local>
2020-03-16 19:16:46 -0700

    added ampac and cite to constructor

--- js/clientConstructor.js
+++ js/clientConstructor.js
@@ -1,3 +1,8 @@
+// Function that can use document.getElementById()
+function documentId(id) {
+    return document.getElementById(id);
+}
+
 // Client Constructor
 function GoogleAdsClient(params) {
     this.currentCost = params.currentCost;
@@ -9,31 +14,51 @@ function GoogleAdsClient(params) {
     }
 }
 
+// Client Constructor prototype method
 GoogleAdsClient.prototype.processBudget = function() {
-    let budgetDifference = this.monthlyCost - document.getElementById(this.currentCost).value;
+    let budgetDifference = this.monthlyCost - documentId(this.currentCost).value;
     let averageMonthlySpend = budgetDifference / daysLeftInMonth();
     this.monthlyCost = Math.round(averageMonthlySpend * 100) / 100;
     this.displayMessage(this.monthlyCost);
 }
 
-let stanfordYouthParams = {
-    monthlyCost: document.getElementById('stanfordMonthlyCost').innerHTML,
-    currentCost: 'costInputStanfordYouthShared',
-    viewId: document.getElementById('displayStanfordYouthBudget')
+// Client object parameters
+const ampacAnalyticalParams = {
+    monthlyCost: documentId('ampacMonthlyCost').innerHTML,
+    currentCost: 'costInputAmpacAnalytical',
+    viewId: documentId('displayAmpacAnalytical')
 };
 
-let stanfordYouthSolutions = new GoogleAdsClient(stanfordYouthParams);
+const stanfordYouthParams = {
+    monthlyCost: documentId('stanfordMonthlyCost').innerHTML,
+    currentCost: 'costInputStanfordYouthShared',
+    viewId: documentId('displayStanfordYouth')
+};
 
-let clients = [stanfordYouthSolutions];
+const citeParams = {
+    monthlyCost: documentId('citeMonthlyCost').innerHTML,
+    currentCost: 'costInputCite',
+    viewId: documentId('displayCite')
+};
 
+// Create new constructor objects
+const ampacAnalytical = new GoogleAdsClient(ampacAnalyticalParams);
+const stanfordYouthSolutions = new GoogleAdsClient(stanfordYouthParams);
+const cite = new GoogleAdsClient(citeParams);
+
+// Save client constructor objects in array
+const clients = [ampacAnalytical, stanfordYouthSolutions, cite];
+
+// Iterate through the clients array and run the processBudget prototype method
 function balanceBudget() {
     for (let i = 0; i < clients.length; i++) {
         clients[i].processBudget();
     }
 }
 
+// Initialize the document to take in the values and show values once button is clicked
 function init() {
-    let submitButton = document.getElementById('stanfordYouthSubmitButton');
+    let submitButton = document.getElementById('newBudgetButton');
     submitButton.onclick = balanceBudget;
 }
 
